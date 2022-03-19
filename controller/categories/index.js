437d3d5052c117b09d5d24224e412fad915a25c1
code-rmc <rodrigoslmatias@gmail.com>
2022-03-19 16:23:54 -0300

    add controller categories

---
+++ controller/categories/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const saveCategory = (req, res, next) => {
+  res.json({});
+};
+
+module.exports = {
+  saveCategory,
+};
