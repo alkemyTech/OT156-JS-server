07bb5623419a4dbd3f4029a06c3befdee48ec5a7
code-rmc <rodrigoslmatias@gmail.com>
2022-03-20 17:09:15 -0300

    add express-validator

--- middleware/categories/index.js
+++ middleware/categories/index.js
@@ -1,14 +1,18 @@
-//  valida el campo name, y el mismo sea un string
-const validCategories = (req, res, next) => {
-  try {
-    const { name } = req.body;
+const { check, validationResult } = require('express-validator');
 
-    if (name && typeof name === 'string') next();
+//  valida el campo name, y que el mismo sea un string
+const validatorCategories = [
+  check('name', 'Name is required').exists().notEmpty(),
+  check('name', 'Name must be string').isString(),
+  (req, res, next) => {
+    try {
+      validationResult(req).throw();
+      next();
+    } catch (errors) {
+      res.status(403);
+      res.send({ errors });
+    }
+  },
+];
 
-    throw Error('Name is required');
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports = validCategories;
+module.exports = validatorCategories;
