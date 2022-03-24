b8ff7d5264589751c3f67b2a9727045254a5a991
Adrián Rodíguez <Appxims@gmail.com>
2022-03-24 07:40:52 -0300

    indentation inprovements

--- routes/organization.js
+++ routes/organization.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/1/public', function(req, res, next) {
-  res.json({
-    name :"ONG Somos más",
-    image:"https://somosmas.org/wp-content/uploads/2018/02/nuevo-logo.png", 
-    phone:"1160112988", 
-    address:"Barrio la cava",
-    welcomeText:"Somos una asociacion civil que se creo en 1997"
-  });
-}); 
+router.get( '/1/public' , function( req , res , next ) {
+  res.json( {
+    name: "ONG Somos más" ,
+    image: "https://somosmas.org/wp-content/uploads/2018/02/nuevo-logo.png" , 
+    phone: "1160112988" , 
+    address: "Barrio la cava" ,
+    welcomeText: "Somos una asociacion civil que se creo en 1997"
+  } ) ;
+} ) ; 
 
 module.exports = router;
