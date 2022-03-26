5f00a5aeb2b36a1461ac1e919a752e55e826171d
nitovillafuerte <nitovillafuerte@outlook.com>
2022-03-26 03:24:39 -0300

    endponit GET /auth/me y services de JWT

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const { Router } = require("express");
+const router = Router();
+const authRoutes = require("./authRoutes");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
+router.use("/auth", authRoutes);
 module.exports = router;
