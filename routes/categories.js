ddd7e9411b1094c4716e45cf07f8c6c4089e5258
code-rmc <rodrigoslmatias@gmail.com>
2022-03-31 00:11:59 -0300

    add route get categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const validatorCategories = require('../middlewares/categoriesMiddlewares');
-const { saveCategory } = require('../controllers/categoriesControllers');
+const {
+  saveCategory,
+  getAllCategories,
+} = require('../controllers/categoriesControllers');
 
 const router = Router();
 
 router.post('/', validatorCategories, saveCategory);
+router.get('/', getAllCategories);
 
 module.exports = router;
