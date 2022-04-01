5957f6e9ace0c651dcee393d342258534ff0e3cd
Adrián Rodíguez <Appxims@gmail.com>
2022-04-01 19:17:01 -0300

    fix Activites

--- models/activities.js
+++ models/activities.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Activities.belongsTo(models.user, {as: 'user'});
+      Activities.belongsTo(models.User, {as: 'User'});
     }
   };
   Activities.init({
