d80d099f1108daf6f9c785b41a9bae1bae1c01fc
Adrián Rodíguez <Appxims@gmail.com>
2022-03-31 21:06:17 -0300

    creacon de controller y middleware

---
+++ controllers/organizationControllers.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const updateOrganization = (req, res, next) => {
+    
+  res.json({});
+};
+
+module.exports = {
+  updateOrganization,
+};
