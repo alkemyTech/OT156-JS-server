a5ab55a58b1d73fec36e27c2b8be1e0c0e655986
AdrianInsomnio <appxims@gmail.com>
2022-04-03 20:18:50 -0300

    migrations and model organization (#19)
    crear migracion modelo organizacion
    
    
    Adjuntar
    
    Vincular incidencia
    
    General
    No modificar
    Descripción
    creacion de modelo organization con campos de name, image,phone, address ,welcomeText

--- models/organization.js
+++ models/organization.js
@@ -3,24 +3,25 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Activities extends Model {
+  class Organization extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Activities.belongsTo(models.User, {as: 'User'});
+      // define association here
     }
   };
-  Activities.init({
+  Organization.init({
     name: DataTypes.STRING,
     image: DataTypes.STRING,
-    content: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
+    phone: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    welcomeText: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Activities',
+    modelName: 'Organization',
   });
-  return Activities;
-}; 
+  return Organization;
+};
