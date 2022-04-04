364a872803d51de6cf0e5a245e558cae8187b532
code-rmc <rodrigoslmatias@gmail.com>
2022-04-04 15:48:34 -0300

    add route backoffice

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { getAllUsersForCamps } = require('../controllers/userControllers');
-const { validatorAdmin } = require('../middlewares/adminMiddlewares');
 
 const router = express.Router();
 
@@ -8,7 +6,5 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-
-router.get('/backoffice/users', validatorAdmin, getAllUsersForCamps);
 
 module.exports = router;
