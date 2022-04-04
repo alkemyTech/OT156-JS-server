291ac21e2790a241b8fbaaf26af279f9e33b7eae
Adrián Rodíguez <Appxims@gmail.com>
2022-04-04 11:52:40 -0300

    add testimonialServices

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const validatorTestimonal = require('../middlewares/testimonialsMiddlewares');
-const { saveTestimonial } = require('../controllers/testimonialControllers');
+const { saveTestimonial } = require('../controllers/testimonialController');
 
 const router = Router();
 
