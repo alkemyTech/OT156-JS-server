51aa5d88b6c62b62627d27cb575844121d9802a3
Joniruiz <ruiz.jonathan2018@gmail.com>
2022-04-05 01:39:26 -0300

    add controler and service with logic

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const validatorAdmin = require('../middlewares/adminMiddlewares');
 const {getAllUsers} = require('../controllers/adminController');
 
-router.get('/users', getAllUsers);
+router.get('/users',validatorAdmin,getAllUsers);
 
 module.exports = router;
