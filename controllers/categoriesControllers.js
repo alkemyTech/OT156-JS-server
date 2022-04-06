927086d4569068e411116ea95c69fbf625bbcf19
code-rmc <rodrigoslmatias@gmail.com>
2022-04-06 16:49:40 -0300

    add save remove and getbyid of category

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { create, getById, remove } = require('../services/categoriesServices');
 
 /**
  *
@@ -6,10 +7,40 @@ const express = require('express');
  * @param {express.Response} res
  * @param {express.NextFunction} next
  */
-const saveCategory = (req, res, next) => {
-  res.json({});
+const saveCategory = async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const newCategory = { name, description };
+    const category = await create(newCategory);
+    res.json(category);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getCategoryById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const category = await getById(id);
+    if (!!category) res.status(404).json({ error: user });
+    res.json({ category });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeCategory = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await remove(id);
+    res.json({ category: 'Category deleted' });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   saveCategory,
+  getCategoryById,
+  removeCategory,
 };
