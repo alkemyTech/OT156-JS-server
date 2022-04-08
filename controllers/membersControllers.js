40933e16c0e7cd2f2baffc0fa84c8912d84298ec
Marlon De La Roche <marlondelaroch3@gmail.com>
2022-04-08 17:55:15 -0500

    Squashed commit of the following:
    commit e98982c90f321bd63633ad876cb48e6bbeacfe88
    Author: Marlon De La Roche <marlondelaroch3@gmail.com>
    Date:   Thu Apr 7 13:46:02 2022 -0500
    
        eliminar nodemon
    
    commit ba973363b70639e8367c2a034e20634734649290
    Author: Marlon De La Roche <marlondelaroch3@gmail.com>
    Date:   Thu Apr 7 13:44:35 2022 -0500
    
        validar string con express-validator
    
    commit af21a212e5187a6e718dd2238826677d944105ad
    Author: Marlon De La Roche <marlondelaroch3@gmail.com>
    Date:   Wed Apr 6 14:11:36 2022 -0500
    
        actualización de miembros lista
    
    commit 54952f96b29ad623526be49665aba9a8e840ebd0
    Author: Marlon De La Roche <marlondelaroch3@gmail.com>
    Date:   Wed Apr 6 13:34:48 2022 -0500
    
        Eliminar miembros
    
    commit 21f024385efc98d210977f3328c43728608020d4
    Author: Marlon De La Roche <marlondelaroch3@gmail.com>
    Date:   Wed Apr 6 12:39:41 2022 -0500
    
        crear ruta para creación de miembros

--- controllers/membersControllers.js
+++ controllers/membersControllers.js
@@ -3,11 +3,17 @@ const {
   getAllServices,
   deleteMember,
 } = require('../services/membersServices.js');
+const { validationResult } = require("express-validator");
 
 const createMember = async (req, res, next) => {
   const { name } = req.body;
-  const memberCreated = await create(name);
-  res.send(memberCreated);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(500).send("Must be a string");
+  } else {
+    const memberCreated = await create(name);
+    res.send(memberCreated);
+  }
 };
 
 const deleteMembers = async (req, res, next) => {
@@ -26,10 +32,20 @@ const getAllMembers = async (req, res, next) => {
     res.status(500);
     res.json({ err });
   }
+}
+
+const updateMembers = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, image } = req.body;
+  const memberUpdated = await updateMember(id, name, image);
+  memberUpdated === 500
+    ? res.status(memberUpdated).send("Member Doesn't Exist")
+    : res.status(memberUpdated).send("Member Updated");
 };
 
 module.exports = {
   createMember,
   deleteMembers,
   getAllMembers,
+  updateMembers,
 };
