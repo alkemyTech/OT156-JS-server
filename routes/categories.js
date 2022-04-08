a845b0daabc6d254055a46e955fa8e496e3e184a
code-rmc <rodrigoslmatias@gmail.com>
2022-04-08 15:35:36 -0300

    add route update

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
 const validatorCategories = require('../middlewares/categoriesMiddlewares');
-const { saveCategory } = require('../controllers/categoriesControllers');
+const { validatorAdmin } = require('../middlewares/adminMiddlewares');
+const {
+  saveCategory,
+  updateCategory,
+} = require('../controllers/categoriesControllers');
 
 const router = Router();
 
 router.post('/', validatorCategories, saveCategory);
+router.put('/:id', validatorAdmin, updateCategory);
+/* TODO agregar validacion de existente */
 
 module.exports = router;
