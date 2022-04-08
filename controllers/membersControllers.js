295a7ce3a4174611349acf3a6482f2e59ecb1a46
code-rmc <rodrigoslmatias@gmail.com>
2022-04-08 15:50:12 -0300

    fix members

--- controllers/membersControllers.js
+++ controllers/membersControllers.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const { getAllServices ,create, deleteMember  } = require('../services/membersServices.js');
-const { create, deleteMember } = require("../services/membersServices");
+const {
+  getAllServices,
+  create,
+  deleteMember,
+} = require('../services/membersServices');
 
 const createMember = async (req, res, next) => {
   const { name } = req.body;
@@ -13,18 +15,18 @@ const deleteMembers = async (req, res, next) => {
   const memberDeleted = await deleteMember(id);
   memberDeleted === 500
     ? res.status(memberDeleted).send("Member Doesn't Exist")
-    : res.status(memberDeleted).send("Member Deleted");
+    : res.status(memberDeleted).send('Member Deleted');
 };
 
-const getAllMembers = async ( req, res, next) => {
-    try {
-        const members = await getAllServices();
-        res.json( { members } );
-    } catch ( err ) {
-        res.status( 500 );
-        res.json( { err } );
-    }
-}
+const getAllMembers = async (req, res, next) => {
+  try {
+    const members = await getAllServices();
+    res.json({ members });
+  } catch (err) {
+    res.status(500);
+    res.json({ err });
+  }
+};
 
 module.exports = {
   createMember,
