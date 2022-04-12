563997484f0aa3f5bff0aba764f672990c15c1ac
Nazareno <nazareno7870@gmail.com>
2022-04-12 15:55:37 -0300

    fix: users route reorganization

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { deleteUser } = require('../controllers/usersControllers');
+const validatorAuth = require('../middlewares/auth');
+const authController = require('../controllers/auth');
+
 
 const router = express.Router();
 
@@ -7,6 +10,7 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/register', validatorAuth, authController.create);
 
 router.delete('/:id', deleteUser);
 
