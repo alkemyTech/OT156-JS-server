0c1739d716aeeb629bcc7cacd9c101a05fe30f59
Nazareno <nazareno7870@gmail.com>
2022-04-15 11:40:13 -0300

    Registry update. Logged in when registering

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const Models = require("../models");
+const { login } = require('../services/loginServices');
 
 const create = async (req, res, next) => {
 
@@ -13,12 +14,20 @@ const create = async (req, res, next) => {
   }
 
   try {
-
+    const password = req.body.password
     req.body.password = bcrypt.hashSync(req.body.password, 10);
-    const data = await Models.User.create(req.body);
-    console.log(data)
-    res.status(200).json({ data });
+    const newUser = {...req.body,
+      roleId: 2,
+      image:''
+    }
 
+    await Models.User.create(newUser);
+    const user = await login({ email:newUser.email, password });
+    if (user.token) {
+      res.send(user);
+    } else {
+      res.status(401).json({ error: user });
+    }
   } catch (e) {
     next(e);
   }
