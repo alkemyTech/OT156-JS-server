dad955f9c44aabad130c8705ae43887e13fe1522
code-rmc <rodrigoslmatias@gmail.com>
2022-04-16 19:32:15 -0300

    refactor middleware

--- middlewares/testimonialsMiddlewares.js
+++ middlewares/testimonialsMiddlewares.js
@@ -1,18 +1,11 @@
-const { check, validationResult } = require('express-validator');
+const { check } = require('express-validator');
+const commonMiddleware = require('../utils/commonMiddleware');
 
 const validatorTestimonal = [
   check('name', 'Name is required').exists().notEmpty(),
   check('name', 'Name must be string').isString(),
   check('content', 'Content is required').exists().notEmpty(),
-  ( req, res, next) => {
-    try {
-      validationResult( req ).throw();
-      next();
-    } catch ( errors ) {
-      res.status( 403 );
-      res.send( { errors } );
-    }
-  },
+  commonMiddleware,
 ];
 
 module.exports = validatorTestimonal;
