bb2108db5074d47a71ba9dd22393357aa0a1b82e
Nazareno del Rìo <nazareno7870@gmail.com>
2022-04-23 20:27:38 -0300

    Endpoint delete, getall, get activitybyid (#52)

--- models/activities.js
+++ models/activities.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Activities.belongsTo(models.User, {as: 'User'});
+      
     }
   }
   Activities.init(
