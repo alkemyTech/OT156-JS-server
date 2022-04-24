02984fe9fda4522f4388aaeae03649bb247d09b1
Nazareno <nazareno7870@gmail.com>
2022-04-23 23:41:53 -0300

    refactor members and seeder of demo members

--- routes/members.js
+++ routes/members.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const { createMember, deleteMembers, getAllMembers, updateMembers  } = require('../controllers/membersControllers');
 const { check } = require('express-validator');
-const validatorAdmin = require('../middlewares/adminMiddlewares');
 
 const router = express.Router();
 
-router.get( '/', validatorAdmin ,  getAllMembers );
+router.get( '/', getAllMembers );
 router.delete('/:id', deleteMembers)
 router.post('/', [check("name").isString()], createMember);
 router.put('/:id', updateMembers)
